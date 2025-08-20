35f03852c93ab47e3579755b33362bd7bc12bbdc
Waltteri Heino <waltteriheino@gmail.com>
2025-08-20 17:13:58 +0300

    Added: t_input struct for storing the input line. Contains original string, split string, count of strings

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhirvasm <mhirvasm@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 10:08:48 by mhirvasm          #+#    #+#             */
-/*   Updated: 2025/08/19 11:33:50 by mhirvasm         ###   ########.fr       */
+/*   Updated: 2025/08/20 17:13:03 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "colors.h"
+
+typedef struct s_input {
+	char	*raw;		// Original line the user typed in.
+	char	**word;		// Array of words (split from the original line).
+	int		count;		// Number of words.
+}		t_input;
 
 void	getworkindir(char *buf, size_t size);
 
