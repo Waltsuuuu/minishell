54219a1bcdd8055591498d655a355a7ab9a77c74
Waltteri Heino <waltteriheino@gmail.com>
2025-08-20 18:35:39 +0300

    Added: Simple input parser.

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 10:08:48 by mhirvasm          #+#    #+#             */
-/*   Updated: 2025/08/20 17:13:03 by wheino           ###   ########.fr       */
+/*   Updated: 2025/08/20 17:53:18 by wheino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 typedef struct s_input {
 	char	*raw;		// Original line the user typed in.
-	char	**word;		// Array of words (split from the original line).
+	char	**words;		// Array of words (split from the original line).
 	int		count;		// Number of words.
 }		t_input;
 
