0af8542edc94a620d180ea11de74c1a90ec51729
Waltteri Heino <waltteriheino@gmail.com>
2025-09-11 17:56:01 +0300

    Refactored build_and_append_redir()

--- include/pipeline.h
+++ include/pipeline.h
@@ -50,21 +50,25 @@ int	get_redir_type(TokType type, t_redir_type *t_type);
 int	is_redir_tok(TokType type);
 int	count_pipes(t_input *input);
 
+// pipeline_args.c
 int	arg_ll_to_arr(t_seg *seg, t_pipeline *pipeline, int cmd_i);
 int	append_arg(t_token *tokens, int i, t_seg *seg);
+
+// pipeline_redirs.c
 int	build_and_append_redir(t_token *tokens, int i, t_seg *seg);
+int	fill_redir_fields(t_redir *redir, t_token *tokens, int i);
 
 // exit_build_pipeline.c
-int	err_exit_build_pipeline(t_pipeline *pipeline, t_seg *seg, int built_count);
+int		err_exit_build_pipeline(t_pipeline *pipeline, t_seg *seg, int built_count);
 void	free_partial_seg(t_seg *seg);
 void	del_redir(void *content);
 void	free_partial_pipeline(t_pipeline *pipeline, int	built_count);
 
-// printer - debugger
-void	print_cmds(const t_pipeline *pipeline);
-void	print_one_cmd(const t_command *cmd, int idx);
-void	print_redirs(const t_list *lst);
-const char	*redir_type_name(t_redir_type t);
+// // printer - debugger
+// void	print_cmds(const t_pipeline *pipeline);
+// void	print_one_cmd(const t_command *cmd, int idx);
+// void	print_redirs(const t_list *lst);
+// const char	*redir_type_name(t_redir_type t);
 
 
 #endif
--- src/build_commands/pipeline_redirs.c
+++ src/build_commands/pipeline_redirs.c
@@ -5,30 +5,16 @@ int	build_and_append_redir(t_token *tokens, int i, t_seg *seg)
 	t_redir			*redir;
 	t_list			*node;
 
-	redir = malloc(sizeof(t_redir));
+	redir = ft_calloc(1, sizeof(t_redir));
 	if (!redir)
 		return (-1);
-	if (get_redir_type(tokens[i].type, &redir->type) == -1)	// set type
+	if (fill_redir_fields(redir, tokens, i) == -1)
 	{
+		if (redir->target)
+			free(redir->target);
 		free(redir);
 		return (-1);
 	}
-	redir->target = ft_strdup(tokens[i + 1].text);			// set target
-	if (!redir->target)
-	{
-		free(redir);
-		return (-1);
-	}
-	if (redir->target[0] == '\0' && redir->type != REDIR_HEREDOC)	// empty target check (Can be empty for heredoc)
-	{
-		free(redir->target);
-		free(redir);
-		printf("minishell: ambiguous redirect\n");
-		return (-1);
-	}
-	redir->no_expand = 0;												// set no_expand
-	if (redir->type == REDIR_HEREDOC && tokens[i + 1].was_quoted == 1)	// set no_expand 1 if heredoc and delim was quoted
-		redir->no_expand = 1;
 	node = ft_lstnew(redir);
 	if (!node)
 	{
@@ -37,5 +23,22 @@ int	build_and_append_redir(t_token *tokens, int i, t_seg *seg)
 		return (-1);
 	}
 	ft_lstadd_back(&seg->redirs, node);
+	return (0);
+}
+
+int	fill_redir_fields(t_redir *redir, t_token *tokens, int i)
+{
+	if (get_redir_type(tokens[i].type, &redir->type) == -1)	// set type
+		return (-1);
+	redir->target = ft_strdup(tokens[i + 1].text);			// set target
+	if (!redir->target)
+		return (-1);
+	if (redir->target[0] == '\0' && redir->type != REDIR_HEREDOC)	// empty target check (Can be empty for heredoc)
+	{
+		printf("minishell: ambiguous redirect\n");	// again this needs to go to stderr
+		return (-1);
+	}
+	if (redir->type == REDIR_HEREDOC && tokens[i + 1].was_quoted == 1)	// set no_expand 1 if heredoc and delim was quoted
+		redir->no_expand = 1;
 	return (0);
 }
