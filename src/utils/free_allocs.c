6fb716ebbd92b470cdd16597e382caf3cb69091d
Waltteri Heino <waltteriheino@gmail.com>
2025-09-18 14:14:24 +0300

    Hotfix for segfault. Added null check before calling free_pipeline

--- src/utils/free_allocs.c
+++ src/utils/free_allocs.c
@@ -3,9 +3,11 @@
 void	free_allocs(t_shell *shell)
 {
 	clear_struct_on_failure(&shell->input);
-	free_pipeline(&shell->pipeline, shell->pipeline.n_cmds);
 	if (shell->pipeline.cmds)
+	{
+		free_pipeline(&shell->pipeline, shell->pipeline.n_cmds);
 		free(shell->pipeline.cmds);
+	}
 	shell->pipeline.cmds = NULL;
 	free(shell->buf);
 }
