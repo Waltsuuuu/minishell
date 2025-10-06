5cb1c3180db74063b2159157206b5c8814812d23
Waltteri Heino <waltteriheino@gmail.com>
2025-10-06 13:32:25 +0300

    quote NORM OK

--- src/00_check_quote_balance/quote_check.c
+++ src/00_check_quote_balance/quote_check.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote_check.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 13:31:55 by wheino            #+#    #+#             */
+/*   Updated: 2025/10/06 13:31:56 by wheino           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 /**
- * @brief Ensures the quotes in the input line are balanced. If no closing quote is found
- * the input is discarded, error message is written, last status is set to 2 (syntax error),
+ * @brief Ensures the quotes in the input line are balanced.
+ * If no closing quote is found the input is discarded,
+ * and an error message is written, last status is set to 2 (syntax error),
  * and user is returned to prompt.
  * @return 0 if balance, -1 if unbalanced. 
  */
-int check_quote_balance(char **line, int *last_status)
+int	check_quote_balance(char **line, int *last_status)
 {
 	if (!line || !*line)
 		return (-1);
@@ -15,7 +28,7 @@ int check_quote_balance(char **line, int *last_status)
 		ft_putstr_fd("minishell: unclosed quote\n", 2);
 		free(*line);
 		*line = NULL;
-		*last_status = 2; // Syntax error (2)
+		*last_status = 2;
 		return (-1);
 	}
 	return (0);
@@ -46,4 +59,3 @@ int	quotes_unbalanced(const char *string)
 	}
 	return (in_single || in_double);
 }
-
