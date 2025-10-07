8845a61bdb9d567019038c25aafc36e4f1b874b6
Waltteri Heino <waltteriheino@gmail.com>
2025-10-07 17:23:57 +0300

    02 expansion NORMINETTE OK

--- include/expand.h
+++ include/expand.h
@@ -16,11 +16,14 @@ int		expand_tokens(t_input *input, int last_status, char **envp);
 /* 					02_expand_status.c							*/
 char	*expand_status(char *text, int last_status);
 int		create_exp_status_text(char *text, char **exp_text, char *status_str);
+int	loop_rebuild_expand(char *text, char **exp_text, char *status_str,
+	t_expand_state *st);
 
 /* 					03_expand_variable.c						*/
 char	*expand_variable(char *text, char **envp);
 int		create_exp_var_text(char *text, char **exp_text, char **envp);
-int		handle_var_expansion(char *text, char **exp_text, int *i, char **envp, int in_single, int in_double);
+int	handle_var_expansion(t_expand_state *st, char *text,
+	char **exp_text, char **envp);
 int		process_var_expansion(char *text, char **exp_text, int *i, char **envp);
 
 /* 					04_expand_utils.c							*/
@@ -33,7 +36,7 @@ int		process_char(char **exp_text, char c);
 /* 					05_expand_utils2.c							*/
 void	init_expand_state(t_expand_state *st);
 size_t	copy_n_chars(char *dst, const char *src, size_t size);
-int		extract_key(char *text, int *i, char **key, int *key_len, int *start_i);
+char	*extract_key(char *text, int *i, int *key_len, int *start_i);
 int		find_env_index(char **envp, char *key, int key_len);
 
 #endif
--- src/02_exapand_vars_and_status/05_expand_utils_2.c
+++ src/02_exapand_vars_and_status/05_expand_utils_2.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   05_expand_utils_2.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wheino <wheino@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 14:54:51 by wheino            #+#    #+#             */
+/*   Updated: 2025/10/07 17:22:48 by wheino           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	init_expand_state(t_expand_state *st)
@@ -27,11 +39,13 @@ size_t	copy_n_chars(char *dst, const char *src, size_t size)
 }
 
 /**
- * @brief Extract variable name (KEY) at *i and return malloc'ed "KEY=".
+ * @brief Extract $VAR (KEY) at *i and return malloc'ed "KEY=".
  * @return 0 on success, -1 on allocation error.
  */
-int	extract_key(char *text, int *i, char **key, int *key_len, int *start_i)
+char	*extract_key(char *text, int *i, int *key_len, int *start_i)
 {
+	char	*key;
+
 	*start_i = *i;
 	*key_len = 0;
 	while (valid_cont_char(text[*i]) == 1)
@@ -39,17 +53,18 @@ int	extract_key(char *text, int *i, char **key, int *key_len, int *start_i)
 		(*i)++;
 		(*key_len)++;
 	}
-	*key = malloc(*key_len + 2); // +2 for '=' and '\0'
-	if (!*key)
-		return (-1);
-	copy_n_chars(*key, &text[*start_i], *key_len);
-	(*key)[*key_len] = '=';
-	(*key)[*key_len + 1] = '\0';
-	return (0);
+	key = malloc(*key_len + 2);
+	if (!key)
+		return (NULL);
+	copy_n_chars(key, &text[*start_i], *key_len);
+	key[*key_len] = '=';
+	key[*key_len + 1] = '\0';
+	return (key);
 }
 
 /**
- * @brief Return index of env entry matching "KEY=", or -1 if absent.
+ * @brief Searches for matching KEY inside the environment array.
+ * @return Index of matching key in environment array, -1 if no match found.
  */
 int	find_env_index(char **envp, char *key, int key_len)
 {
