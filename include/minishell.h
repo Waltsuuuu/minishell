2d588c3a503026807301138b47a31dafad3b2a22
Waltteri Heino <waltteriheino@gmail.com>
2025-10-08 18:41:23 +0300

    removed comment

--- include/minishell.h
+++ include/minishell.h
@@ -8,7 +8,7 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <sys/stat.h> // access and macros
+# include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <signal.h>
