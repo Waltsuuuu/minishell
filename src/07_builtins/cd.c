09b43f03a1626a8981617ada8261e3b516e570df
Miika <mhirvasm@studen.hive.fi>
2025-10-16 13:03:54 +0300

    cd error messages fixed

--- src/07_builtins/cd.c
+++ src/07_builtins/cd.c
@@ -6,11 +6,38 @@
 /*   By: mhirvasm <mhirvasm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 12:04:50 by mhirvasm          #+#    #+#             */
-/*   Updated: 2025/10/06 12:20:51 by mhirvasm         ###   ########.fr       */
+/*   Updated: 2025/10/16 13:03:23 by mhirvasm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	cd_error_and_return(char **argv)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(argv[0], STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	if (errno == ENOENT)
+	{
+		ft_putstr_fd("No such file or directory\n", STDERR_FILENO);
+		return (1);
+	}
+	else if (errno == EACCES)
+	{
+		ft_putstr_fd("No permission\n", STDERR_FILENO);
+		return (1);
+	}
+	else if (errno == ENOTDIR)
+	{
+		ft_putstr_fd(": is not a directory\n", STDERR_FILENO);
+		return (1);
+	}
+	else
+	{
+		ft_putstr_fd("Undefined cd error\n", STDERR_FILENO);
+		return (1);
+	}
+}
 
 int	builtin_cd(char **argv, t_shell *shell)
 {
@@ -31,7 +58,7 @@ int	builtin_cd(char **argv, t_shell *shell)
 	if (!getcwd(oldbuf, sizeof(oldbuf)))
 		ft_putstr_fd("cd: getwcd\n", 2);
 	if (chdir(path) < 0)
-		return (ft_putstr_fd("cd error\n", 2), 1);
+		return (cd_error_and_return(argv));
 	if (!getcwd(newbuf, sizeof(newbuf)))
 		return (ft_putstr_fd("getwcd error\n", 2), 1);
 	if (env_set(&shell->env_head, "OLDPWD", oldbuf) == 1)
